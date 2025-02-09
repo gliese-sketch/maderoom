48ba2444aeacfc34a5b071212d4a5f7d2bb55ebb
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-09 09:20:49 +0000

    change state on click

--- client/app/page.js
+++ client/app/page.js
@@ -1,17 +1,21 @@
 "use client";
 import { HeroUIProvider, Button } from "@heroui/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [selected, setSelected] = useState("😀");
+
   const emojis = "😅,😄,😶,😊,😎,😀".split(",");
 
   const handleClick = (emoji) => {
     console.log(emoji);
+    setSelected(emoji);
   };
 
   return (
     <HeroUIProvider>
       <div className="min-h-screen flex justify-center items-center flex-col gap-16">
-        <h1 className="text-7xl">😅</h1>
+        <h1 className="text-7xl">{selected}</h1>
         <div className="flex gap-1 flex-wrap justify-center">
           {emojis.map((emoji, index) => {
             return (
