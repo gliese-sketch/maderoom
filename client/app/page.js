7f6cbe95b6d7b3889fc9dd7be4a7ee307e81f886
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-09 09:14:54 +0000

    add heroUI

--- client/app/page.js
+++ client/app/page.js
@@ -1,3 +1,32 @@
+"use client";
+import { HeroUIProvider, Button } from "@heroui/react";
+
 export default function Home() {
-  return <div>Hello there</div>;
+  return (
+    <HeroUIProvider>
+      <div className="min-h-screen flex justify-center items-center flex-col gap-16">
+        <h1 className="text-7xl">😅</h1>
+        <div className="flex gap-1 flex-wrap justify-center">
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+          <Button variant="bordered" className="text-3xl p-5">
+            😅
+          </Button>
+        </div>
+      </div>
+    </HeroUIProvider>
+  );
 }
