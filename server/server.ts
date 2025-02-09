7315107240537a4d0debb921af70a21e548c8bda
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-09 08:13:44 +0000

    add io connection

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,28 @@
-console.log("Hello There!");
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+
+const app = express();
+
+const PORT = 8000;
+const server = http.createServer(app);
+
+// MARK: API Route
+app.get('/', (req, res) => {
+    res.send("Socket.io server is healthy!")
+})
+
+// MARK: Socket.io
+const io = new Server(server, {
+    cors: {
+        origin: "*" // For CORS
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Some one connected", socket)
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is up and running on PORT:${PORT}`)
+})
