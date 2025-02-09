a75e3ddbd57451125398f7fefbeab0fae6805487
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-09 09:19:49 +0000

    add button click logic

--- client/app/page.js
+++ client/app/page.js
@@ -2,29 +2,29 @@
 import { HeroUIProvider, Button } from "@heroui/react";
 
 export default function Home() {
+  const emojis = "😅,😄,😶,😊,😎,😀".split(",");
+
+  const handleClick = (emoji) => {
+    console.log(emoji);
+  };
+
   return (
     <HeroUIProvider>
       <div className="min-h-screen flex justify-center items-center flex-col gap-16">
         <h1 className="text-7xl">😅</h1>
         <div className="flex gap-1 flex-wrap justify-center">
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
-          <Button variant="bordered" className="text-3xl p-5">
-            😅
-          </Button>
+          {emojis.map((emoji, index) => {
+            return (
+              <Button
+                key={index}
+                variant="bordered"
+                className="text-3xl p-5"
+                onPress={() => handleClick(emoji)}
+              >
+                {emoji}
+              </Button>
+            );
+          })}
         </div>
       </div>
     </HeroUIProvider>
